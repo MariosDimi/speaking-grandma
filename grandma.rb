2d4b3e3fcf9a1d9f1ba30b7662d60061c58ad89c
Marios <dimitropoulosmarios@hotmail.com>
2020-10-31 22:01:37 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -13,17 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-    if phrase == "I LOVE YOU GRANDMA!"
-        return "I LOVE YOU TOO PUMPKIN!"
-      elsif phrase == phrase.downcase
-            return "HUH?! SPEAK UP, SONNY!"
-      elsif phrase == phrase.downcase
-        return "HUH?! SPEAK UP, SONNY!"
-      elsif phrase == phrase.upcase
-        return "NO, NOT SINCE 1938!"
-      elsif
-        return "I LOVE YOU TOO PUMPKIN!"
-    end
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
-
-speak_to_grandma("HELLO THERE")
